41ed69f48285811a1ffd938e9057fa3ae4704e5e
BTWitsK <BTWitsK@Gmail.com>
2021-11-22 19:37:22 -0600

    stage two complete

--- Cinema.java
+++ Cinema.java
@@ -1,11 +1,28 @@
 package cinema;
+import java.util.Scanner;
 
 public class Cinema {
-
     public static void main(String[] args) {
-        String[][] cinema = new String [8][9];
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Cinema:");
+        System.out.println("Enter the number of rows:");
+        int rows = scanner.nextInt();
+        System.out.println("Enter the number of seats in each row:");
+        int seatsPerRow = scanner.nextInt();
+
+        if (rows * seatsPerRow <= 60) {
+            System.out.println("Total income:");
+            System.out.printf("$%d", rows * seatsPerRow * 10);
+        } else {
+            int frontHalf = rows / 2;
+            int frontPrice = frontHalf * seatsPerRow * 10;
+            int backPrice = (rows - frontHalf) * seatsPerRow * 8;
+            System.out.println("Total income:");
+            System.out.printf("$%d", frontPrice + backPrice);
+        }
+    }
+
+        /* System.out.println("Cinema:");
         for (int i = 0; i < cinema.length; i++) {
             for (int j = 0; j < cinema[i].length; j++) {
                 if (i == 0 && j == 0){
@@ -23,5 +40,5 @@ public class Cinema {
             System.out.println();
         }
 
-    }
+    }*/
 }
