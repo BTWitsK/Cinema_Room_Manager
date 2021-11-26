4e9ce8ac33459df50ca52cba2e84dda700a0a98d
BTWitsK <BTWitsK@Gmail.com>
2021-11-26 00:56:45 -0600

    finished stage five

--- Cinema.java
+++ Cinema.java
@@ -12,6 +12,7 @@ public class Cinema {
 
         String[][] cinema = new String[rows + 1][seatsPerRow + 1];
         int cinemaSize = rows * seatsPerRow;
+        int totalIncome = ((rows / 2) * seatsPerRow * 10) + ((rows - (rows / 2)) * seatsPerRow * 8);
 
         System.out.println("Cinema:");
         for (int i = 0; i < cinema.length; i++) {
@@ -34,7 +35,7 @@ public class Cinema {
 
         while (!exit) {
 
-            System.out.print("1. Show the seats\n2. Buy a ticket\n0. Exit \n");
+            System.out.print("1. Show the seats\n2. Buy a ticket\n3. Statistics\n0. Exit \n");
             int input = scanner.nextInt();
 
             switch (input) {
@@ -44,6 +45,10 @@ public class Cinema {
                 case 2:
                     buyTicket(cinema, cinemaSize, rows);
                     printArray(cinema);
+
+                    break;
+                case 3:
+                    printStats(cinema, cinemaSize, totalIncome);
                     break;
                 case 0:
                     exit = true;
@@ -56,11 +61,29 @@ public class Cinema {
     }
     public static void buyTicket(String [][] theater, int theaterSize, int rows) {
         Scanner scanner = new Scanner(System.in);
+        int row;
+        int seat;
+        boolean repeatInput;
 
-        System.out.println("Enter a row number:");
-        int row = scanner.nextInt();
-        System.out.println("Enter a seat number in that row:");
-        int seat = scanner.nextInt();
+        do {
+            repeatInput = false;
+            System.out.println("Enter a row number:");
+            row = scanner.nextInt();
+            System.out.println("Enter a seat number in that row:");
+            seat = scanner.nextInt();
+
+            if (row < 1 || row > theater.length - 1) {
+                System.out.println("Wrong input!");
+                repeatInput = true;
+            } else if (seat < 1 || seat > theater[0].length - 1) {
+                System.out.println("Wrong input!");
+                repeatInput = true;
+            } else if ("B ".equals(theater[row][seat])) {
+                System.out.println("That ticket has already been purchased!");
+                repeatInput = true;
+            }
+
+        } while (repeatInput);
 
         int ticketPrice = 10;
         boolean backSeat = row > (rows / 2);
@@ -83,4 +106,28 @@ public class Cinema {
         }
     }
 
-}
+    public static void printStats(String [][] theater, int theaterSize, int total) {
+        int purchasedTickets = 0;
+        int income = 0;
+        int rows = 0;
+        for (String[] Rows: theater) {
+            for (String Seats : Rows) {
+                if ((Seats).contains("B")) {
+                    purchasedTickets++;
+                    if (rows <= ((theater.length - 1) / 2)) {
+                        income += 10;
+                    } else {
+                        income += 8;
+                    }
+
+                }
+            }
+            rows++;
+        }
+            System.out.printf("Number of purchased tickets: %d\n", purchasedTickets);
+            System.out.printf("Percentage: %.2f%%\n", (float)purchasedTickets / theaterSize * 100);
+            System.out.printf("Current income: $%d\n", income);
+            System.out.printf("Total income: $%d\n", total);
+
+        }
+    }
