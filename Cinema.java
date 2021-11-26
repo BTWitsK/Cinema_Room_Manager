d11f9c15835b30c59250c4bb4c0a802090dd124d
BTWitsK <BTWitsK@Gmail.com>
2021-11-25 21:18:23 -0600

    finished stage four

--- Cinema.java
+++ Cinema.java
@@ -11,6 +11,7 @@ public class Cinema {
         int seatsPerRow = scanner.nextInt();
 
         String[][] cinema = new String[rows + 1][seatsPerRow + 1];
+        int cinemaSize = rows * seatsPerRow;
 
         System.out.println("Cinema:");
         for (int i = 0; i < cinema.length; i++) {
@@ -29,7 +30,33 @@ public class Cinema {
             System.out.println();
         }
 
-        int cinemaSize = rows * seatsPerRow;
+        boolean exit = false;
+
+        while (!exit) {
+
+            System.out.print("1. Show the seats\n2. Buy a ticket\n0. Exit \n");
+            int input = scanner.nextInt();
+
+            switch (input) {
+                case 1:
+                    printArray(cinema);
+                    break;
+                case 2:
+                    buyTicket(cinema, cinemaSize, rows);
+                    printArray(cinema);
+                    break;
+                case 0:
+                    exit = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+    public static void buyTicket(String [][] theater, int theaterSize, int rows) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Enter a row number:");
         int row = scanner.nextInt();
         System.out.println("Enter a seat number in that row:");
@@ -38,19 +65,22 @@ public class Cinema {
         int ticketPrice = 10;
         boolean backSeat = row > (rows / 2);
 
-        if (cinemaSize > 60 && backSeat) {
+        if (theaterSize > 60 && backSeat) {
             ticketPrice = 8;
         }
 
         System.out.printf("Ticket price: $%d\n", ticketPrice);
-        cinema[row][seat] = "B ";
+        theater[row][seat] = "B ";
+    }
 
+    public static void printArray(String [][] theater) {
         System.out.println("Cinema:");
-        for (String[] Rows: cinema) {
+        for (String[] Rows: theater) {
             for (String Seats: Rows) {
                 System.out.print(Seats);
             }
             System.out.println();
         }
     }
+
 }
